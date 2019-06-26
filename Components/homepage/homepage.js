af72f079dccac990225bc7b68664fcf4de3fe760
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-06-25 21:25:12 -0700

    tried to add a carousel element - sort of works but will try to fix later

---
+++ Components/homepage/homepage.js
@@ -0,0 +1,58 @@
+class Carousel {
+    constructor(item) {
+
+        this.item = item;
+        this.rightBtn = item.querySelector(".right-button");
+        this.leftBtn = item.querySelector(".left-button");
+
+        this.images = Array.from(item.querySelectorAll(".carousel-image"));
+
+        this.currentIndex = 0;
+        this.defaultImage = this.images[this.currentIndex];
+        this.defaultImage.style.display = "block";
+
+        //Events on Button
+        this.rightBtn.addEventListener("click", () => this.showNextImg());
+        this.leftBtn.addEventListener("click", () => this.showBeforeImg());
+
+        }
+
+
+        showNextImg() {
+            let allImages = document.querySelectorAll(".carousel-image");
+
+            allImages.forEach(function (img) {
+                img.style.display = "none";
+            });
+
+            this.currentIndex +- 1;
+
+            if (this.currentIndex > allImages.length - 1) {
+                this.currentIndex = 0;
+            }
+
+            allImages[this.currentIndex].style.display = "block";
+            }
+
+
+        showBeforeImg() {
+            let allImages = document.querySelectorAll(".carousel-image");
+
+            allImages.forEach(function (img) {
+                img.style.display = "none";
+            });
+
+            this.currentIndex -=1;
+
+            if (this.currentIndex < 0) {
+                this.currentIndex = 3;
+            }
+
+            allImages[this.currentIndex].style.display = "block";
+            }
+        }
+
+
+
+
+let carousel = new Carousel (document.querySelector(".carousel"));
